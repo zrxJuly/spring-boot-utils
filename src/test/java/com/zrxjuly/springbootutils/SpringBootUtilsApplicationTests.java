5f3010c5a6d8bd86129400747622e8735e91da65
rongxiang <zrxJuly@qq.com>
2019-04-08 11:34:53 +0800

    project initialize

---
+++ src/test/java/com/zrxjuly/springbootutils/SpringBootUtilsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zrxjuly.springbootutils;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootUtilsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
