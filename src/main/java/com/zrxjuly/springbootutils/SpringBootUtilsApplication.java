8876c840ea96a9f227a98a16f1172e7a22f0fc32
rongxiang <zrxJuly@qq.com>
2019-04-08 14:02:41 +0800

    add docs

--- src/main/java/com/zrxjuly/springbootutils/SpringBootUtilsApplication.java
+++ src/main/java/com/zrxjuly/springbootutils/SpringBootUtilsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootUtilsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootUtilsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootUtilsApplication.class, args);
+    }
 
 }
